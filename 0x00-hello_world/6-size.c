4b29e3a7bcd88176c3b402035faa4b118b4cf643
kelvin ode <ode.kelvin@gmail.com>
2023-03-21 13:45:00 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,5 @@ int main(void)
 	printf("Size of a int: %lu byte(s)", sizeof(int));
 	printf("Size of a long int: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float))e;
+	printf("Size of a float: %lu byte(s)", sizeof(float));
 }
