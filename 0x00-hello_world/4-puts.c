543dcf0e6cbc75709738f95582e50da6f8f66f2d
kelvin ode <ode.kelvin@gmail.com>
2023-03-21 11:54:24 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
- /**
-  * main - this is the main function
-  * 
-  * Return: always 0
-  */
+/**
+ * main - this is the main function
+ *  
+ * Return: always 0
+ */
 
 int main(void)
 {
