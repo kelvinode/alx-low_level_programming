0fb6b3b50724d89d4b0085aad5071ea5448e3e2b
kelvin ode <ode.kelvin@gmail.com>
2023-03-21 18:46:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,21 @@
 /**
  * main - main function
  *
- * Return: 0 on Success
+ * Return: 0 on success
  */
+
 int main(void)
 {
-       char a;
-       int b;
-       long int c;
-       long long int d;
-       float f;
-	
-printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float f;
+
+ printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+ printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+ printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+ printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+ printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+ return (0);
 }
